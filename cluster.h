064a50940d42ca1049af743dd01c8d7caa8e64aa
wesley boyett <wes@wesleys-MacBook-Air.local>
2023-02-19 08:30:49 -0800

    simplified.

--- cluster.h
+++ cluster.h
@@ -8,25 +8,34 @@
 #include <vector>
 #include <set>
 
-std::vector<std::set<int>> cluster(std::vector<std::vector<float>> input, int clusters, int redos) {
-    int dimension = input[0].size();
+std::vector<std::set<int>> cluster(std::vector<std::vector<float>> input, int numClusters, int redos) {
+    int dimensions = input[0].size();
     int size = input.size();
     std::vector<float> conc;
     for(std::vector<float> datapoint : input) {
-        conc.insert( conc.end(), datapoint.begin(), datapoint.end() );
+        conc.insert( conc.end(), datapoint.begin(), datapoint.end());
     }
     faiss::ClusteringParameters params;
     params.nredo = redos;
-    faiss::Clustering clus(dimension, clusters, params);
+    faiss::Clustering clus(dimensions, numClusters, params);
     std::unique_ptr<faiss::Index> index;
-    index.reset(new faiss::IndexFlatL2(dimension));
+    index.reset(new faiss::IndexFlatL2(dimensions));
     clus.train(size, conc.data(), *index.get());
     std::cout << index->ntotal << std::endl;
 
+    faiss::IndexFlatL2 cIndex(dimensions);
     std::vector<float> dis(size);
     std::vector<faiss::idx_t> idx(size);
+    
+    cIndex.add(numClusters, clus.centroids.data());
+    cIndex.search(size, conc.data(), 1, dis.data(), idx.data());
 
-    index->search(size, conc.data(), clusters, dis.data(), idx.data());
     std::vector<std::set<int>> result;
+    for(int i = 0; i < numClusters; i++) {
+        result.emplace_back(std::set<int>());
+    }
+    for(int i = 0; i < size; i++) {
+        result[idx[i]].insert(i);
+    }
     return result;
 }
