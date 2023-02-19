41d2e00ac66d53a17b782c4ad9a43a994692a1c3
wesboyt <wesboyt@github.com>
2023-02-19 08:43:23 -0800

    updates.

--- cluster.h
+++ cluster.h
@@ -8,7 +8,7 @@
 #include <vector>
 #include <set>
 
-std::vector<std::set<int>> cluster(std::vector<std::vector<float>> input, int numClusters, int redos) {
+std::vector<std::vector<int>> cluster(std::vector<std::vector<float>> input, int numClusters, int retry) {
     int dimensions = input[0].size();
     int size = input.size();
     std::vector<float> conc;
@@ -16,7 +16,7 @@ std::vector<std::set<int>> cluster(std::vector<std::vector<float>> input, int nu
         conc.insert( conc.end(), datapoint.begin(), datapoint.end());
     }
     faiss::ClusteringParameters params;
-    params.nredo = redos;
+    params.nredo = retry;
     faiss::Clustering clus(dimensions, numClusters, params);
     std::unique_ptr<faiss::Index> index;
     index.reset(new faiss::IndexFlatL2(dimensions));
@@ -30,12 +30,12 @@ std::vector<std::set<int>> cluster(std::vector<std::vector<float>> input, int nu
     cIndex.add(numClusters, clus.centroids.data());
     cIndex.search(size, conc.data(), 1, dis.data(), idx.data());
 
-    std::vector<std::set<int>> result;
+    std::vector<std::vector<int>> result;
     for(int i = 0; i < numClusters; i++) {
-        result.emplace_back(std::set<int>());
+        result.emplace_back(std::vector<int>());
     }
     for(int i = 0; i < size; i++) {
-        result[idx[i]].insert(i);
+        result[idx[i]].emplace_back(i);
     }
     return result;
 }
--- example.cpp
+++ example.cpp
@@ -7,10 +7,17 @@ int main() {
     std::mt19937 gen(rnd());
     std::uniform_real_distribution<> dist(0.,1.);
 
-    for(int i = 0; i < 200; i++) {
-        auto temp = std::vector<float>(12);
+    for(int i = 0; i < 10000; i++) {
+        auto temp = std::vector<float>(512);
         std::generate(temp.begin(), temp.end(), [&gen, &dist]() { return dist(gen); });
         inputs.emplace_back(temp);
     }
     auto clusters = cluster(inputs, 3, 50);
+    for(int i = 0; i < clusters.size(); i++) {
+        std::cout << "cluster " << i << ": ";
+        for(int id : clusters[i]){
+            std::cout << id << " ";
+        }
+        std::cout << std::endl;
+    }
 }
